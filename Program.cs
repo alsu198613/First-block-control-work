b8b8025268891e412b23e7e823712630248902bc
Alexandra Sukneva <alsu198613@gmail.com>
2023-05-01 19:50:09 +0300

    added Program.cs

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿Console.Write("Введите элементы массива через пробел: ");
+string[] input = Console.ReadLine().Split(' ');
+string[] output = new string[input.Length];
+
+NewArrayWith3(input, output);
+PrintArray(output);
+
+void NewArrayWith3(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+        if (array1[i].Length <= 3)
+        {
+            array2[count] = array1[i];
+            count++;
+        }
+    }
+}
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
